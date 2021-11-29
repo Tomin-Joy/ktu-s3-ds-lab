ca7df1541c8b54076189c7ac71d3a9aa4af361d9
Tomin Joy <67215154+Tomin-Joy@users.noreply.github.com>
2021-11-29 14:33:35 +0530

    sparse
    Element [0][0] : 2
    Element [0][1] : 0
    Element [0][2] : 5
    Element [1][0] : 0
    Element [1][1] : 0
    Element [1][2] : 3
    Element [2][0] : 0
    Element [2][1] : 2
    Element [2][2] : 0
    3  3  4  
    0  0  2  
    0  2  5  
    1  2  3  
    2  1  2

---
+++ sparse.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+void readMatrix(int A[][10],int m,int n){
+	printf("Enter the elements\n\n");
+	for(int i = 0;i<m;i++){
+		for(int j = 0;j<n;j++){
+			printf("Element [%d][%d] : ",i,j);
+			scanf("%d",&A[i][j]);
+		}
+	}
+}
+
+int  sparse(int A[][10],int B[][10],int m,int n){
+	int k=0;
+	B[0][0] = m;
+	B[0][1] = n;
+	for(int i=0;i<m;i++){
+		for(int j = 0;j<n;j++){
+			if(A[i][j]!=0){
+				k++;
+				B[k][0] = i;
+				B[k][1] = j;
+				B[k][2] = A[i][j];
+				
+			}
+		}
+	}
+	B[0][2] = k;
+	return k;
+}
+
+void printMatrix(int A[][10],int m,int n){
+	for(int i = 0;i<=m;i++){
+		for(int j= 0;j<n;j++){
+			printf("%d  ",A[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+void main(){
+	int A[10][10],Sparse[10][10],m,n;
+	printf("Enter the number of rows : ");
+	scanf("%d",&m);
+	printf("Enter the number of columns : ");
+	scanf("%d",&n);
+	readMatrix(A,m,n);
+	int k = sparse(A,Sparse,m,n);
+	printMatrix(Sparse,k,3);
+}
+	  
