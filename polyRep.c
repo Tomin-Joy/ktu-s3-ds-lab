844445028b71ea0e88c1620da97345775512a398
Tomin-Joy <tominjk007@gmail.com>
2021-12-16 07:42:54 +0530

    ಠ_ಠ

--- polyRep.c
+++ polyRep.c
@@ -1,31 +1,43 @@
 #include<stdio.h>
+typedef struct term
+{
+    int coef;
+    int expo;
+}term;
+
 int main(){
-    int exp[40],n,power,flag = 0;
+    int n,power;
+    term expression[20];
     printf("Enter the number of elements  : ");
     scanf("%d",&n);
-    for(int i=0;i<40;i++)
-    	exp[i] = 0;
-    for(int i=n;i>0;i--){
+    for(int i=n-1;i>=0;i--){
     	printf("\nEnter the power : ");
-    	scanf("%d",&power);
-        printf("Enter the coefficient of x^%d : ",power);
-        scanf("%d",&exp[power]);
-    }
+    	scanf("%d",&expression[i].expo);
+        printf("Enter the coefficient of x^%d : ",expression[i].expo);
+        scanf("%d",&expression[i].coef);
+    }for(int i=0;i<n;i++){
+		int min=i;
+		for(int j=i+1;j<n;j++){
+			if(expression[min].expo>expression[j].expo){
+				min=j;
+            }	
+		}
+		if(i!=min){
+			term temp=expression[i];
+			expression[i]=expression[min];
+			expression[min]=temp;
+		}
+	}
     printf("\n\nThe expression is : ");
-    for(int i=39;i>=0;i--){
-    	if(exp[i]==0){
-    		continue;
+    printf("%d(x^%d) ",expression[n-1].coef,expression[n-1].expo);
+    for(int i=n-2;i>=0;i--){
+        
+    	if (expression[i].coef<0){
+    		printf(" - %d(x^%d)",expression[i].coef * -1,expression[i].expo);
     	}
     	else{
-    	if (flag==0){
-    		printf("%d(x^%d) ",exp[i],i);
-    		flag =1;
-    	}
-    	else{
-    	printf("+ %d(x^%d) ",exp[i],i);
+    	    printf(" + %d(x^%d)",expression[i].coef,expression[i].expo);
+        }
     }
-    }
-    
-}
     return 0;
 }
